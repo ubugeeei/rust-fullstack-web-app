d97c7e272cf3a2df3baf7b7c1036775c9794c2ae
Ubugeeei <ubuge1122@gamil.com>
2022-07-03 01:25:55 +0900

    faet: todo view sample

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,48 @@
 use yew::prelude::*;
 
+pub struct Todo {
+    pub id: i32,
+    pub title: String,
+    pub description: String,
+    pub is_done: bool,
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    // TODO: fetching
+    let todos: UseStateHandle<Vec<Todo>> = use_state(|| {
+        vec![
+            Todo {
+                id: 1,
+                title: "Learn Rust".to_string(),
+                description: "Learn Rust".to_string(),
+                is_done: false,
+            },
+            Todo {
+                id: 2,
+                title: "Learn Yew".to_string(),
+                description: "Learn Yew".to_string(),
+                is_done: false,
+            },
+        ]
+    });
+
     html! {
-        <h1>{ "Hello World" }</h1>
+        <div>
+            <h1>{ "Todo App !" }</h1>
+            <ul>{ todos
+                .iter()
+                .map(|todo| {
+                    html! {
+                        <li>
+                            <h2>{todo.title.to_string()}</h2>
+                            <p>{ todo.description.to_string() }</p>
+                            <p>{ todo.is_done.to_string() }</p>
+                        </li>
+                    }
+                })
+                .collect::<Html>() }</ul>
+        </div>
     }
 }
 
