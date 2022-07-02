ef978fddfb437ce84f7ed8c0b2fac5261fe635d1
Ubugeeei <ubuge1122@gamil.com>
2022-07-02 23:19:23 +0900

    fix: entity field type

--- backend/src/todo/entities.rs
+++ backend/src/todo/entities.rs
@@ -10,13 +10,13 @@ static SEQUENCE_ID: Lazy<Mutex<i32>> = Lazy::new(|| Mutex::new(0));
 pub struct Todo {
     pub id: i32,
     pub title: String,
-    pub description: Option<String>,
+    pub description: String,
     pub is_done: bool,
 }
 
 impl Todo {
     #[allow(dead_code)]
-    fn new(title: String, description: Option<String>) -> Todo {
+    fn new(title: String, description: String) -> Todo {
         let mut id = SEQUENCE_ID.lock().unwrap();
         *id += 1;
         Todo {
